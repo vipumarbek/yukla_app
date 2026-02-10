f7ed9e6a533bcf10ca906e9d6774fdf3c3e15826
Umarbek VIP <vipumarbek@gmail.com>
2026-02-10 12:17:30 +0500

    home_screen va api_service yangilandi

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'SALOM, FLUTTER ISHLAYAPTI',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Yukla App',
+      home: const HomeScreen(),
+    );
+  }
 }
