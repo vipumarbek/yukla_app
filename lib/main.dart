a34139b8ed543be76be7392b1633d433ca0080d9
Umarbek VIP <vipumarbek@gmail.com>
2026-02-11 10:25:18 +0500

    home_screen va api_service yangilandi

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
-// lib/main.dart
-
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomeScreen(),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'api_service.dart';
+import '../services/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
